08821c6963b8281ccd6a391cb04f367bbb37d42b
JT Rael <raeljohntimothy@gmail.com>
2024-04-12 12:49:40 +0800

    Maps and lists

--- Main.dart
+++ Main.dart
@@ -12,7 +12,7 @@ void main() {
   ];
 
   final teenFilter = listOfPeople.where((person) => person.age < 20).toList();
-  // print(teenFilter);
+  //print(teenFilter.toString());
 
   // SET: removed all the existing repeating elements
   final john = Person(21, name: "John");
@@ -24,5 +24,25 @@ void main() {
     john,
     john
   };
-  print(setOfPeople.toString()); // Matt, Mark, Luke, John
+  //print(setOfPeople.toString()); // Matt, Mark, Luke, John
+
+  // MAP: key value pairs
+  Map<String, int> marks = {"Math": 90, "English": 89, "Science": 95};
+  marks["Marketing"] = 90; // Allowed. UNLIKE in List this will error
+  // print(marks);
+  marks.remove("Math");
+  // print(marks);
+
+  // LIST with MAP
+  List<Map<String, int>> subjectMarks = [
+    {"Geometry": 90, "Physics": 87},
+    {"JAVA": 92, "C": 90},
+    {"Data Structures": 88, "Algorithm": 97}
+  ];
+
+  subjectMarks.map((e) {
+    e.forEach((key, value) {
+      print('$key: $value');
+    });
+  }).toList();
 }
